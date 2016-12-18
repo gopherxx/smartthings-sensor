ea74afdf43d45fe5e876d2310075c35822a60c50
gopherxx <gopherxx.git@gmail.com>
2016-12-17 20:20:13 -0500

    updated water sensor

--- smartthings-sensor/water-sensor-esp8266.groovy
+++ smartthings-sensor/water-sensor-esp8266.groovy
@@ -2,7 +2,7 @@
  preferences {
     input("ip", "text", title: "IP Address", description: "ip", required: true)
     input("port", "number", title: "Port", description: "port", default: 9060, required: true)
-    //input("mac", "text", title: "MAC Addr", description: "mac")
+    input("mac", "text", title: "MAC Addr", description: "mac")
 }
  
  metadata {
@@ -144,4 +144,28 @@ def refresh() {
         ]
     )
 }
-  
+  // polltemp()
+    //pollcontact()
+
+
+ //def pollcontact() {
+ //log.debug "Executing 'pollcontact' ${getHostAddress()}"
+ //new physicalgraph.device.HubAction(
+ //       method: "GET",
+ //       path: "/getstatus",
+ //       headers: [
+ //              HOST: "${getHostAddress()}"
+ //     ]
+ //   )
+//}
+
+
+//def polltemp() {
+//	log.debug "Executing 'polltemp' ${getHostAddress()}"
+//	new physicalgraph.device.HubAction(
+//   	method: "GET",
+//    	path: "/getTemp",
+//    	headers: [
+//        	HOST: "${getHostAddress()}"
+//    	]
+//	)
